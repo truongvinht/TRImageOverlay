c5a727e7dd5e20aede03d7549ce574eecb66389f
Truong Vinh Tran <truongvinht@gmail.com>
2020-08-03 12:41:14 +0200

    fix sample configuration

--- Example/TRImageOverlay/ViewController.swift
+++ Example/TRImageOverlay/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import TRImageOverlay
 
 class ViewController: UIViewController {
-
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        print("Write to \(paths[0].absoluteString)")
+        return paths[0]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // init new overlay
+        let overlay = TRImageOverlay(with: CGSize(width: 1000, height: 1000))
+        
+        guard let img = overlay.generate(content: "ZXM", with: 10) else {
+            print("image not generate")
+            return
+        }
+        
+        if let jpegData = img.pngData() {
+            let name = getDocumentsDirectory().appendingPathComponent("test-TRImageOverlay.png")
+            try? jpegData.write(to: name)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
--- TRImageOverlay/Classes/TRImageOverlay.swift
+++ TRImageOverlay/Classes/TRImageOverlay.swift
@@ -36,25 +36,25 @@ public class TRImageOverlay {
     private var size: CGSize
     
     /// background color, default is white
-    private var bgColor: UIColor = UIColor.white
+    var bgColor: UIColor = UIColor.clear
     
     /// alpha value of overlay, default value is 1
-    private var alpha: Float = 1.0
+    var alpha: CGFloat = 1.0
     
     
     // MARK: - Content
     
     /// font with default size 12
-    private var font: UIFont = UIFont.systemFont(ofSize: 12)
+    var font: UIFont = UIFont.systemFont(ofSize: 12)
     
     /// text color
-    private var fgColor: UIColor = UIColor.black
+    var fgColor: UIColor = UIColor.black
     
     /// number of generated text items
     private var number: Int = 10
     
-    /// random orientation flag
-    private var randomized: Bool = false
+    /// random orientation degree
+    private var degree: Double = 0.0
     
     public init (with size: CGSize) {
         self.size = size
@@ -64,15 +64,58 @@ public class TRImageOverlay {
         
         let frame = CGRect(origin: CGPoint.zero, size: self.size)
         
-        let nameLabel = UILabel(frame: frame)
-        nameLabel.textAlignment = .left
-        nameLabel.backgroundColor = self.bgColor
-        nameLabel.textColor = self.fgColor
-        nameLabel.font = self.font
-        nameLabel.text = content
+        // use tmp label for getting size
+        let tmpLabel = UILabel()
+        tmpLabel.font = self.font
+        tmpLabel.text = content
+        
+        let fontSize = tmpLabel.intrinsicContentSize
+        
+
+        print("Base: \(fontSize)")
+        
         UIGraphicsBeginImageContext(frame.size)
-         if let currentContext = UIGraphicsGetCurrentContext() {
-            nameLabel.layer.render(in: currentContext)
+         if let context = UIGraphicsGetCurrentContext() {
+            
+            let entries = numberOfColumnsAndRows(within: frame.size, objectSize: fontSize)
+
+            let paragraphStyle: NSMutableParagraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = NSTextAlignment.left
+            
+            var fColor = self.fgColor
+            var bColor = self.bgColor
+            
+            if UIColor.clear != fColor && self.alpha < 1 {
+                fColor = self.fgColor.withAlphaComponent(self.alpha)
+            }
+            
+            if UIColor.clear != bColor && self.alpha < 1 {
+                bColor = self.bgColor.withAlphaComponent(self.alpha)
+            }
+            
+            
+            let attributes = [
+                NSAttributedString.Key.foregroundColor: fColor,
+                NSAttributedString.Key.backgroundColor: bColor,
+                NSAttributedString.Key.font: self.font,
+                NSAttributedString.Key.paragraphStyle: paragraphStyle
+            ]
+            context.saveGState()
+            //context.translateBy(x: 500, y: 500)
+            //context.rotate(by: 20 * CGFloat(M_PI / 180))
+            
+            for row in 0...entries.x {
+                for column in 0...entries.y {
+
+                    
+                    let text = NSString(string: content)
+                    let labelFrame = position(within: frame.size, objectSize: fontSize, row: row, column: column)
+                    
+                    text.draw(at: labelFrame.origin, withAttributes: attributes)
+                }
+            }
+            context.restoreGState()
+            
             let nameImage = UIGraphicsGetImageFromCurrentImageContext()
             
             guard let image = nameImage else {
@@ -83,5 +126,27 @@ public class TRImageOverlay {
          }
         
         return nil
+    }
+    
+    func numberOfColumnsAndRows(within: CGSize, objectSize: CGSize) -> (x: Int, y: Int) {
+        
+        if CGSize.zero.equalTo(objectSize) {
+            return (1, 1)
+        }
+        
+        // x axis, columns
+        let columns = Int((within.width / 2) / objectSize.width)
+        let rows = Int((within.height / 2) / objectSize.height)
+        
+        return (columns, rows)
+    }
+    
+    func position(within: CGSize, objectSize: CGSize, row: Int, column: Int) -> CGRect {
+        
+        var result = CGRect(origin: CGPoint.zero, size: objectSize)
+        
+        result.origin.x = objectSize.width * CGFloat(row * 2)
+        result.origin.y = objectSize.height * CGFloat(column * 2)
+        return result
     }
 }
